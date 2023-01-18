9e96fa4067de5f3ca124092616bf986bac632834
Alex Schmidt <alex.schmidt767@gmail.com>
2023-01-17 19:42:52 -0600

    solution 2 finished

--- solution2.py
+++ solution2.py
@@ -1,1 +1,14 @@
 # Write code for algorithm 2 below
+def natural_numbers(n, i=1):
+    # error case
+    if n < i:
+        print(f'Number must be larger than {i - 1}')
+    # base case
+    elif i > n:
+        return
+    # recursive case
+    else:
+        print(i)
+        natural_numbers(n, i + 1)
+
+natural_numbers(5)
