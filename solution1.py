098d12839017a335ba854e99c759b43ba5ef64a7
Alex Schmidt <alex.schmidt767@gmail.com>
2023-01-17 19:38:04 -0600

    solution 1 comments added

--- solution1.py
+++ solution1.py
@@ -1,10 +1,14 @@
 # Write code for algorithm 1 below
 def count_down(n):
+    # error case
     if n < 0:
+        print('No negative numbers!')
         return
+    # base case
     elif n == 0:
         print(n)
         return
+    # recursive case
     else:
         print(n)
         count_down(n - 1)
