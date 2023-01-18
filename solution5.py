18a6bbae0de535c2ca7c9776346ff0a019567095
Alex Schmidt <alex.schmidt767@gmail.com>
2023-01-17 20:12:07 -0600

    solution 5 finished

--- solution5.py
+++ solution5.py
@@ -1,1 +1,11 @@
 # Write code for algorithm 5 below
+def is_palindrome(string):
+    # base case
+    if len(string) == 1 or len(string) == 0:
+        return True
+    # recursive case
+    return (string[0] == string[-1]) and is_palindrome(string[1:-1])
+
+print(is_palindrome('racecar'))
+print(is_palindrome('sara'))
+print(is_palindrome('g'))
