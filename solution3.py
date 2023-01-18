a1371aab25543295f5ad552b07fbb23f3f7bd9b5
Alex Schmidt <alex.schmidt767@gmail.com>
2023-01-17 19:57:00 -0600

    solution 3 finished

--- solution3.py
+++ solution3.py
@@ -1,1 +1,16 @@
 # Write code for algorithm 3 below
+def fibonacci_sequence(n):
+    # error case
+    if n <= 0:
+        print('Input must be greater or equal to zero.')
+        return
+    # base cases
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    # recursive case
+    else:
+        return fibonacci_sequence(n - 1) + fibonacci_sequence(n - 2)
+
+print(fibonacci_sequence(30))
