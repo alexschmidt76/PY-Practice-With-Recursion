899a345a6331146f97b1c075813d64189b5b0469
Alex Schmidt <alex.schmidt767@gmail.com>
2023-01-17 19:58:44 -0600

    extra test cases added to solution 3

--- solution3.py
+++ solution3.py
@@ -3,7 +3,6 @@ def fibonacci_sequence(n):
     # error case
     if n <= 0:
         print('Input must be greater than zero.')
-        return
     # base cases
     elif n == 1:
         return 0
@@ -13,4 +12,6 @@ def fibonacci_sequence(n):
     else:
         return fibonacci_sequence(n - 1) + fibonacci_sequence(n - 2)
 
+print(fibonacci_sequence(-9))
+print(fibonacci_sequence(1))
 print(fibonacci_sequence(30))
