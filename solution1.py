71fd45ad28fd977f32ccd81afe6c2d7170821b97
Alex Schmidt <alex.schmidt767@gmail.com>
2023-01-17 19:37:01 -0600

    solution 1 finished

--- solution1.py
+++ solution1.py
@@ -1,1 +1,12 @@
 # Write code for algorithm 1 below
+def count_down(n):
+    if n < 0:
+        return
+    elif n == 0:
+        print(n)
+        return
+    else:
+        print(n)
+        count_down(n - 1)
+
+count_down(30)
